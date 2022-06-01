b9ef5c3e32638eac67bf95db933c311d480ff9dc
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2022-06-01 15:25:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,13 +8,13 @@ function setup() {
   createCanvas(600, 400);
   background(0);
   
-  rectx = int(random(1, width - 100));
-  recty = int(random(1, height - 50));
-  xspeed = int(random(-5, 6));
-  yspeed = int(random(-5, 6));
+  rectx = random(1, width - 100);
+  recty = random(1, height - 50);
+  xspeed = random(-5, 6);
+  yspeed = random(-5, 6);
   while (xspeed === 0 || yspeed === 0) {
-    xspeed = int(random(-5, 6));
-    yspeed = int(random(-5, 6));
+    xspeed = random(-5, 6);
+    yspeed = random(-5, 6);
   }
 }
 
